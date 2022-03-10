cb3aae2be989faaaf39b54b6dff551db2ece6807
jason51102000 <jason51102000@gmail.com>
2022-03-10 17:03:56 +0800

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    eslint: true,
+    esmExternals: false,
     jsconfigPaths: true, // enables it for both jsconfig.json and tsconfig.json
   },
   images: {
     domains: [
       'firebasestorage.googleapis.com', 
     ],
+    loader: "imgix",
+    path: "",
   },
+  assetPrefix: "./",
+
   reactStrictMode: true,
 }
 
