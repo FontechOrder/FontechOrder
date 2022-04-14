8f76fc058d6332d7789c09a84a1e5f65718d30a7
jason51102000 <jason51102000@gmail.com>
2022-04-14 16:50:17 +0800

    add page/table

--- src/other-support/Database/db.ts
+++ src/other-support/Database/db.ts
@@ -23,3 +23,41 @@ export const requestUsers = async () => {
 
   return jsonData
 }
+
+export const requestNewUser = async ({
+  name,
+  email,
+  type,
+}: {
+  name: string
+  email: string
+  type: string
+}) => {
+  const response = await fetch(
+    `http://localhost:3000/api/requestNewUser?name=${name}&email=${email}&type=${type}`
+  )
+
+  const jsonData = await response.json()
+
+  return jsonData
+}
+
+export const createUserTable = async () => {
+  const response = await fetch(
+    'http://localhost:3000/api/database/createUserTable'
+  )
+
+  const jsonData = await response.json()
+
+  return jsonData
+}
+
+export const dropUserTable = async () => {
+  const response = await fetch(
+    'http://localhost:3000/api/database/dropUserTable'
+  )
+
+  const jsonData = await response.json()
+
+  return jsonData
+}
--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -4,22 +4,7 @@ import PageContentDefault from '@containers/PageContent/default'
 import OrderList from '@components/OrderList'
 import NewOrdersLink from '@components/NewOrdersLink'
 
-import { requestUsers } from '@other-support/Database/db'
-
 const Order = () => {
-  React.useEffect(() => {
-    const asyncRequestUsers = async () => {
-      try {
-        const users = await requestUsers()
-        console.log('users: ', users)
-      } catch {
-        console.log('asyncRequestUsers fail.')
-      }
-    }
-
-    asyncRequestUsers()
-  }, [])
-
   return (
     <PageContentDefault>
       <NewOrdersLink />
